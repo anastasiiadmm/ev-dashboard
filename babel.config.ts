7ab9ec4bd75c418308917fba55ed6138b70ac2e4
Anastasiia Ponama <anastasiiadmm@gmail.com>
2023-10-06 15:15:38 +0600

    EV-429 - fixed babel plugin's errors

--- babel.config.ts
+++ babel.config.ts
@@ -4,4 +4,9 @@ module.exports = {
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-typescript',
   ],
+  plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
