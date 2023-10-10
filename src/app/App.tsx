ecab2b7c8318ed8bd4d64224f7c89e7d0ade709d
Anastasiia Ponama <anastasiiadmm@gmail.com>
2023-10-10 19:11:19 +0600

    EV-185 - deleted React dependency

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { Route, RouteObject, Routes, useRoutes } from 'react-router';
 import { observer } from 'mobx-react-lite';
 
