0ff9d1a57b13b7e71d90943dc97b1e9e7abe464d
Anastasiia Ponama <anastasiiadmm@gmail.com>
2023-10-25 16:44:52 +0600

    EV-194 - fixed imports

--- src/shared/ui/components/index.ts
+++ src/shared/ui/components/index.ts
@@ -1,10 +1,4 @@
 export { default as LayoutComponent } from './LayoutComponent/LayoutComponent';
 export { default as FormField } from './FormFIeld/FormField';
-export { default as PasswordField } from './Fields/PasswordField/PasswordField';
-export { default as EmailField } from './Fields/EmailField/EmailField';
-export { default as CheckboxField } from './Fields/CheckboxField/CheckboxField';
-export { default as DefaultField } from './Fields/DefaultField/DefaultField';
-export { default as SelectField } from './Fields/SelectField/SelectField';
-export { default as AutoCompleteComponent } from './Fields/AutoCompleteComponent/AutoCompleteComponent';
 export { default as TableComponent } from './TableComponent/TableComponent';
 export { default as PaginationComponent } from './TableComponent/components/PaginationComponent/PaginationComponent';
