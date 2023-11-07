b68b6f5230e34123447bb3c1d639965410b7666b
Anastasiia Ponama <anastasiiadmm@gmail.com>
2023-11-07 12:21:16 +0600

    EV-574 - sat language to local storage

--- src/shared/ui/Fields/LanguageSelect/LanguageSelect.tsx
+++ src/shared/ui/Fields/LanguageSelect/LanguageSelect.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import i18n from 'i18next';
 
 import { eng, kg, rus } from '~/assets/images';
 import { FormField } from '~/shared/ui';
 
 function LanguageSelect() {
-  const [lang, setLang] = useState('ru');
+  const savedLanguage = localStorage.getItem('language') || 'ru';
+  const [lang, setLang] = useState(savedLanguage);
+
+  useEffect(() => {
+    i18n.changeLanguage(savedLanguage);
+  }, [savedLanguage]);
 
   const handleChange = (value: string) => {
     i18n.changeLanguage(value);
     setLang(value);
+    localStorage.setItem('language', value);
   };
 
   return (
